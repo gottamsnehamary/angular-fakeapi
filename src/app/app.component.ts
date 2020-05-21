6a0a238b28ea7ccde59000e5b884444dacc57184
gottamsnehamary <gottamsneha21@gmail.com>
2020-05-21 10:03:58 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app-assigment-one';
+export class AppComponent implements OnInit {
+  title = 'app works!';
+  students: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    let resp = this.http.get('http://localhost:3000/userList');
+    resp.subscribe((data) => (this.students = data));
+  }
 }
